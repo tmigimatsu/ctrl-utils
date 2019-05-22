40d8b68f13bf6a66eb20f7bb71825518dd52f561
Toki Migimatsu <toki.migimatsu@gmail.com>
2019-05-22 15:08:33 -0700

    Update travis script

--- doc/travis.sh
+++ doc/travis.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+cd external
+git clone https://github.com/cpp-redis/cpp_redis.git --branch 4.3.1 --single-branch cpp_redis.git
+cd cpp_redis.git
+sed -i.old -E "s/^GENERATE_TAGFILE( +)=$/GENERATE_TAGFILE\1= cpp_redis.tag/" .doxygen
+doxygen .doxygen
+cd ..
+cd ..
+
 cd doc
 doxygen Doxyfile
