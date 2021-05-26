3d9906d9f0de6405c64c7666b1d755efad99617d
Toki Migimatsu <toki.migimatsu@gmail.com>
2021-05-26 15:24:24 -0700

    Simplify timer

--- include/ctrl_utils/timer.h
+++ include/ctrl_utils/timer.h
@@ -23,8 +23,9 @@ namespace ctrl_utils {
  * @see Python: spatialdyn.Timer
  */
 class Timer {
-
  public:
+  using Seconds = std::chrono::duration<double>;
+  using Clock = std::chrono::steady_clock;
 
   /**
    * Default constructor that sets the loop frequency to 1000 Hz.
@@ -43,48 +44,45 @@ class Timer {
    * @return Timer loop frequency in Hz.
    * @see Python: spatialdyn.Timer.freq
    */
-  double freq() const { return 1e9 / ns_interval_.count(); }
+  double freq() const { return 1 / dt(); }
 
   /**
    * Set the timer loop frequency in Hz.
    */
-  void set_freq(double frequency) {
-    ns_interval_ = std::chrono::nanoseconds(static_cast<unsigned int>(1e9 / frequency));
-  }
+  void set_freq(double frequency) { set_dt(1. / frequency); }
 
   /**
    * @return Timer loop time interval in seconds (1 / frequency).
    * @see Python: spatialdyn.Timer.dt
    */
-  double dt() const { return ns_interval_.count() / 1e9; }
+  double dt() const {
+    return std::chrono::duration_cast<Seconds>(dt_interval_).count();
+  }
 
   /**
    * Set the timer loop time interval in seconds (1 / frequency).
    */
   void set_dt(double dt) {
-    ns_interval_ = std::chrono::nanoseconds(static_cast<unsigned int>(dt / 1e9));
+    dt_interval_ = std::chrono::duration_cast<Clock::duration>(Seconds(dt));
   }
 
   /**
    * @return Current CPU time since epoch in seconds.
    * @see Python: spatialdyn.Timer.time
    */
-  double time() const {
-    auto now = std::chrono::steady_clock::now();
-    auto now_ns = std::chrono::time_point_cast<std::chrono::nanoseconds>(now);
-    auto ns_since_epoch = now_ns.time_since_epoch();
-    return ns_since_epoch.count() / 1e9;
+  double time(bool update = false) const {
+    if (update) t_curr_ = Clock::now();
+    return std::chrono::duration_cast<Seconds>(t_curr_.time_since_epoch())
+        .count();
   }
 
   /**
    * @return CPU time since last timer reset in seconds.
    * @see Python: spatialdyn.Timer.time_elapsed
    */
-  double time_elapsed() const {
-    auto now = std::chrono::steady_clock::now();
-    auto elapsed = now - t_start_;
-    auto ns_elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed);
-    return ns_elapsed.count() / 1e9;
+  double time_elapsed(bool update = false) const {
+    if (update) t_curr_ = Clock::now();
+    return std::chrono::duration_cast<Seconds>(t_curr_ - t_start_).count();
   }
 
   /**
@@ -92,10 +90,13 @@ class Timer {
    * @see Python: spatialdyn.Timer.time_sim
    */
   double time_sim() const {
-    auto elapsed = t_next_ - t_start_;
-    auto ns_elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed);
-    return ns_elapsed.count() / 1e9;
+    return std::chrono::duration_cast<Seconds>(t_next_ - t_start_).count();
   }
+
+  /**
+   * @return Average frequency since last timer reset in Hz.
+   */
+  double average_freq() const { return (num_iters() - 1) / time_elapsed(); }
 
   /**
    * @return Number of loop iterations since last timer reset.
@@ -110,8 +111,9 @@ class Timer {
    */
   void Reset() {
     num_iters_ = 0;
-    t_start_ = std::chrono::steady_clock::now();
-    t_next_  = t_start_ + ns_interval_;
+    t_start_ = Clock::now();
+    t_curr_ = t_start_;
+    t_next_ = t_start_ + dt_interval_;
   }
 
   /**
@@ -120,32 +122,30 @@ class Timer {
    * @see Python: spatialdyn.Timer.sleep()
    */
   void Sleep() {
-    ++num_iters_;
+    t_curr_ = Clock::now();
     if (!initialized_) {
+      t_start_ = t_curr_;
+      t_next_ = t_curr_;
       initialized_ = true;
-      t_start_ = std::chrono::steady_clock::now();
-      t_next_  = t_start_ + ns_interval_;
-      return;
     }
 
-    auto t_curr = std::chrono::steady_clock::now();
-    if (t_curr < t_next_) {
-      std::this_thread::sleep_for(t_next_ - t_curr);
+    if (t_curr_ < t_next_) {
+      std::this_thread::sleep_for(t_next_ - t_curr_);
     }
-    t_next_ += ns_interval_;
+    t_next_ += dt_interval_;
+    ++num_iters_;
   }
 
  private:
-
   /// @cond
-  std::chrono::steady_clock::time_point t_start_;
-  std::chrono::steady_clock::time_point t_next_;
+  Clock::time_point t_start_;
+  mutable Clock::time_point t_curr_;
+  Clock::time_point t_next_;
   unsigned long long num_iters_ = 0;
 
   bool initialized_ = false;
-  std::chrono::nanoseconds ns_interval_ = std::chrono::milliseconds(1);
+  Clock::duration dt_interval_ = std::chrono::milliseconds(1);
   /// @endcond
-
 };
 
 }  // namespace ctrl_utils
