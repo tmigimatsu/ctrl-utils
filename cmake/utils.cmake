123ba3288e384be0e8b0bf213842c8524bdd82e5
Toki Migimatsu <toki.migimatsu@gmail.com>
2021-06-03 11:25:23 -0700

    Fix bug with pip install

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -28,7 +28,9 @@ function(init_git_submodule GIT_SUBMODULE)
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         RESULT_VARIABLE git_submodule_result
     )
-    if(NOT git_submodule_result EQUAL "0")
+    if(git_submodule_result EQUAL "128")
+        message(WARNING "${GIT_EXECUTABLE} submodule update --init ${RECURSIVE} ${GIT_SUBMODULE} failed with error:\n ${git_submodule_result}. Current directory is an orphaned git repo.")
+    elseif(NOT git_submodule_result EQUAL "0")
         message(FATAL_ERROR "${GIT_EXECUTABLE} submodule update --init ${RECURSIVE} ${GIT_SUBMODULE} failed with error:\n ${git_submodule_result}")
     endif()
 endfunction()
